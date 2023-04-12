f9c882aa7e6ac78cfe050e082e2d139eedd72d18
Luana Alves <luanaalvessoares_@outlook.com>
2023-04-12 11:05:21 -0300

    feat: nav-ul animation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
+const navUl = document.querySelector('.nav-ul');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('active');
+    navUl.classList.toggle('active');
 })
+
